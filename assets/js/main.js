b73ad5b0ada87682238501c76cb741859d6fd1ac
TAUFER Kevin <kevin.taufer@hexagon.com>
2023-03-14 10:55:25 +0100

    added styles for mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,16 @@ let activeTheme = 'dark';
 
 
 const themeSwitch = document.getElementById("themeSwitch");
-
+const showMobileMenu = document.getElementById("showMobileMenu");
+const closeModal = document.getElementById("closeModal");
 
 themeSwitch.addEventListener("click", toggleTheme)
+showMobileMenu.addEventListener("click", dropMenu)
+closeModal.addEventListener("click", dropMenu);
 
 function toggleTheme(e) {
     const body = document.body;
   
-    
-
     if(activeTheme == 'dark') {
         activeTheme = 'light'
         themeSwitch.innerHTML = '<i class="fa fa-moon-o"></i>'
@@ -20,7 +21,13 @@ function toggleTheme(e) {
     }
 
     body.classList.toggle("light-mode");
-   
+}
 
-
+function dropMenu() {
+    var mobileMenu = document.querySelector(".mobile-nav");
+    if (mobileMenu.classList.contains("active")) {
+        mobileMenu.classList.remove('active');
+    } else {
+        mobileMenu.classList.add('active');
+    }
 }
